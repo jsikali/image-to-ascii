2e5cfb596d03e83c568ecfdea4b7910198c416c0
jsikali <120147133+jsikali@users.noreply.github.com>
2024-07-06 16:45:38 -0400

    can print ftxts, need to figure out filetype nuances

--- temp.c
+++ temp.c
@@ -1,3 +1,39 @@
-//temp file to figure out what im doing lol
-//lalallalalalaa
-//just typing this to initially commit
+//ok the thought process is just get file, open it
+//dump out all the contents for now and refer to wiki
+//or an rfc. idk. i swear i saw this on wiki at some point
+//lalaalallalalalallalaalalallala
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+   //hope this is called correctly
+   if(argc != 2)
+   {
+      printf("Incorrect arguments. \nUsage: ./temp (file path)\n");
+      return -1;
+   }
+   
+   //hope the arg is a file lol
+   FILE* image = fopen(argv[1], "r");
+   if(image == NULL)
+   {
+      printf("Image not found at \"%s\".", argv[1]);
+      return -1;
+   }
+
+   //just print whatever is there to see
+   printf("printing the image at \"%s\" w/o any parsing...\n\n", argv[1]);
+   //fprintf(stdout, "%s", image); // lol
+
+   char filecontent = fgetc(image);
+	while(filecontent != EOF)
+	{
+		printf("%c", filecontent);
+		filecontent = fgetc(image);
+	}
+	
+
+   fclose(image);
+   return 0;   
+}
