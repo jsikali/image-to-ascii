3ee8cea543a1500231ebe0600e99bfc35117db31
jsikali <120147133+jsikali@users.noreply.github.com>
2024-08-07 15:38:04 -0400

    lol its more broken but the signatures are there kinda

--- temp.c
+++ temp.c
@@ -4,13 +4,14 @@
 //lalaalallalalalallalaalalallala
 
 #include <stdio.h>
+#include <wchar.h> //for unicode i guess
 
 int main(int argc, char** argv)
 {
    //hope this is called correctly
    if(argc != 2)
    {
-      printf("Incorrect arguments. \nUsage: ./temp (file path)\n");
+      printf("Incorrect/unexpected arguments. \nUsage: ./temp (file path)\n");
       return -1;
    }
    
@@ -23,14 +24,14 @@ int main(int argc, char** argv)
    }
 
    //just print whatever is there to see
-   printf("printing the image at \"%s\" w/o any parsing...\n\n", argv[1]);
+   printf("printing the image at \"%s\" w/o any parsing...\n", argv[1]);
    //fprintf(stdout, "%s", image); // lol
 
 	//nts: on jpg this prints nothing, on png the file signature is wrong?
-   char filecontent = fgetc(image);
+   wchar_t filecontent = fgetc(image);
 	while(filecontent != EOF)
 	{
-		printf("%x ", filecontent);
+		printf("%lc ", filecontent);
 		filecontent = fgetc(image);
 	}
 	
